4946a07901fe502edafe3a7aa1c056af3b2ca13b
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-04 15:20:57 -0400

    removed space

--- Base/base.tf
+++ Base/base.tf
@@ -13,7 +13,6 @@ resource "aws_route_table" "public_subnet_route_table" {
 resource "aws_route_table_association" "public_subnet_route_map" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_subnet_route_table.id
-
   depends_on = [aws_route_table.public_subnet_route_table]
 }
 
@@ -26,7 +25,6 @@ resource "aws_route_table" "private_subnet_route_table" {
 resource "aws_route_table_association" "private_subnet_route_map" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_subnet_route_table.id
-
   depends_on = [aws_route_table.private_subnet_route_table]
 }
 
