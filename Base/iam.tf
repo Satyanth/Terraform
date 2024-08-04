72fea91a6e719d304ac2b862d20f17987af74afa
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-04 18:07:16 -0400

    Resolved errors base

--- Base/iam.tf
+++ Base/iam.tf
@@ -1,9 +1,9 @@
 resource "aws_iam_openid_connect_provider" "OIDC_provider" {
   url = var.github_token_url
 
-  client_id_list = [ 
+  client_id_list = [
     var.aws_sts_api,
   ]
 
-  thumbprint_list = [ var.var.github_thumbprint ]
+  thumbprint_list = [var.github_thumbprint]
 }
