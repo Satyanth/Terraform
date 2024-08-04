13c98689124f5e38ba4d8323ee4a1c1025b36bb3
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-04 19:22:38 -0400

    Added region

--- Base/iam.tf
+++ Base/iam.tf
@@ -8,22 +8,22 @@ resource "aws_iam_openid_connect_provider" "OIDC_provider" {
   thumbprint_list = [var.github_thumbprint]
 }
 
-resource "aws_iam_role" "githubactions_role" {
-  name = "github-actions-role"
+# resource "aws_iam_role" "githubactions_role" {
+#   name = "github-actions-role"
 
-  assume_role_policy = jsondecode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.OIDC_provider.arn
-      },
-      Action = "sts:AssumeRoleWithWebIdentity",
-      Condition = {
-        StringEquals = {
-          "token.actions.githubusercontent.com:sub" : "repo:Satyanth/Terraform:ref:refs/heads/main"
-        }
-      }
-    }]
-  })
-}
+#   assume_role_policy = jsondecode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Effect = "Allow",
+#       Principal = {
+#         Federated = aws_iam_openid_connect_provider.OIDC_provider.arn
+#       },
+#       Action = "sts:AssumeRoleWithWebIdentity",
+#       Condition = {
+#         StringEquals = {
+#           "token.actions.githubusercontent.com:sub" : "repo:Satyanth/Terraform:ref:refs/heads/main"
+#         }
+#       }
+#     }]
+#   })
+# }
