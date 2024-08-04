0cd9cefc9cb4a0a03cd6f6f9e163ec00766601cf
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-04 19:24:58 -0400

    Modified role

--- Base/iam.tf
+++ Base/iam.tf
@@ -1,12 +1,12 @@
-resource "aws_iam_openid_connect_provider" "OIDC_provider" {
-  url = var.github_token_url
+# resource "aws_iam_openid_connect_provider" "OIDC_provider" {
+#   url = var.github_token_url
 
-  client_id_list = [
-    var.aws_sts_api,
-  ]
+#   client_id_list = [
+#     var.aws_sts_api,
+#   ]
 
-  thumbprint_list = [var.github_thumbprint]
-}
+#   thumbprint_list = [var.github_thumbprint]
+# }
 
 # resource "aws_iam_role" "githubactions_role" {
 #   name = "github-actions-role"
