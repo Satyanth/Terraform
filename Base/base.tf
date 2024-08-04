cf6c019aeda55e34a10e8aecfe4003b67b4d337c
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-04 14:59:31 -0400

    Added comments in Base.tf file

--- Base/base.tf
+++ Base/base.tf
@@ -1,12 +1,15 @@
+# Creating vpc
 resource "aws_vpc" "vpc" {
   cidr_block = "10.10.0.0/16"
 
 }
 
+#public subnet Route table
 resource "aws_route_table" "public_subnet_route_table" {
   vpc_id = aws_vpc.vpc.id
 }
 
+#Route table mapping to public subnet and route table
 resource "aws_route_table_association" "public_subnet_route_map" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_subnet_route_table.id
@@ -14,10 +17,12 @@ resource "aws_route_table_association" "public_subnet_route_map" {
   depends_on = [aws_route_table.public_subnet_route_table]
 }
 
+#Private subnet route table
 resource "aws_route_table" "private_subnet_route_table" {
   vpc_id = aws_vpc.vpc.id
 }
 
+#Route table mapping to private subnet and route table
 resource "aws_route_table_association" "private_subnet_route_map" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_subnet_route_table.id
@@ -25,12 +30,14 @@ resource "aws_route_table_association" "private_subnet_route_map" {
   depends_on = [aws_route_table.private_subnet_route_table]
 }
 
+#Public subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_CIDR
   map_public_ip_on_launch = true # This condition makes the subnet as public, assigns ipv4 address
 }
 
+#Private subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.private_CIDR
