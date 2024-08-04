1a401d3a60dfdcf1a12d8f33437ec3fb5b39d7ea
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-04 15:04:13 -0400

    Added in outputs

--- Base/outputs.tf
+++ Base/outputs.tf
@@ -9,3 +9,11 @@ output "public_subnet_id" {
 output "private_subnet_id" {
   value = aws_subnet.private_subnet.id
 }
+
+output "public_routetable_id" {
+  value = aws_route_table.public_subnet_route_table.id
+}
+
+output "private_routetable_id" {
+  value = aws_route_table.private_subnet_route_table.id
+}
