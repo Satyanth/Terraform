32d429e639b305f93f01c97df7aeab0800b16eb2
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-05 14:53:58 -0400

    iam file

--- Base/iam.tf
+++ Base/iam.tf
@@ -30,7 +30,7 @@
 # }
 resource "aws_iam_role" "resources_role" {
   name = "deployment-resources-role"
-  aassume_role_policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
