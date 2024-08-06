32081b5ad5c0909b76f8ece1061401693b04e4ea
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-06 17:35:46 -0400

    Creating vpc and subnets, routetable

--- Base/outputs.tf
+++ Base/outputs.tf
@@ -1,19 +1,19 @@
-# output "vpc_id" {
-#   value = aws_vpc.vpc.id
-# }
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
 
-# output "public_subnet_id" {
-#   value = aws_subnet.public_subnet.id
-# }
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
 
-# output "private_subnet_id" {
-#   value = aws_subnet.private_subnet.id
-# }
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
 
-# output "public_routetable_id" {
-#   value = aws_route_table.public_subnet_route_table.id
-# }
+output "public_routetable_id" {
+  value = aws_route_table.public_subnet_route_table.id
+}
 
-# output "private_routetable_id" {
-#   value = aws_route_table.private_subnet_route_table.id
-# }
+output "private_routetable_id" {
+  value = aws_route_table.private_subnet_route_table.id
+}
