1764bff961365d61b7a3565fb2e917a2ec4d3d1e
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-08 23:14:03 -0400

    kube iam.tf file

--- Kube/iam.tf
+++ Kube/iam.tf
@@ -7,8 +7,6 @@ resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
 resource "aws_iam_role" "eks_role" {
   assume_role_policy = templatefile("${path.module}/templates/eks_role_policy.json", { OIDC_ARN = aws_iam_openid_connect_provider.eks_oidc_provider.arn, OIDC_URL = replace(aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", ""), NAMESPACE = "kube-system", SA_NAME = "aws-node" })
   name               = "eks_role"
-
-  depends_on = [aws_iam_openid_connect_provider.eks_oidc_provider]
 }
 
 resource "aws_iam_role_policy_attachment" "aws_node" {
