af8e85f92c135683c58cdd32063229e8cd1bcf78
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-08 21:40:40 -0400

    kube.tf

--- Kube/kubernetes.tf
+++ Kube/kubernetes.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "eks_cluster" {
   name = var.eks-cluster-name
-  role = data.aws_iam_role.github-iam-role.arn
+  role_arn = data.aws_iam_role.github-iam-role.arn
 
   vpc_config {
     subnet_ids = [ var.public_subnet_one_id, var.public_subnet_two_id ]
