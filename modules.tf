7237febe1bf92b397b26f48ad791420376148b3e
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-08 22:09:26 -0400

    Added kube

--- modules.tf
+++ modules.tf
@@ -3,9 +3,9 @@ module "base" {
   githubactions-role = data.aws_iam_role.github-iam-role.id
 }
 
-# module "kube" {
-#   source               = "./Kube"
-#   public_subnet_one_id = module.base.public_subnet_one_id
-#   public_subnet_two_id = module.base.public_subnet_two_id
-#   github-iam-role      = data.aws_iam_role.github-iam-role.arn
-# }
+module "kube" {
+  source               = "./Kube"
+  public_subnet_one_id = module.base.public_subnet_one_id
+  public_subnet_two_id = module.base.public_subnet_two_id
+  github-iam-role      = data.aws_iam_role.github-iam-role.arn
+}
