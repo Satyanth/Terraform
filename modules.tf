cf7c615b376652e81daa3757d50a620f52b2117a
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-08 21:59:34 -0400

    modules.tf

--- modules.tf
+++ modules.tf
@@ -7,5 +7,5 @@ module "kube" {
   source               = "./Kube"
   public_subnet_one_id = module.base.public_subnet_one_id
   public_subnet_two_id = module.base.public_subnet_two_id
-  github-iam-role      = data.aws_iam_role.github-iam-role.id
+  github-iam-role      = data.aws_iam_role.github-iam-role.arn
 }
