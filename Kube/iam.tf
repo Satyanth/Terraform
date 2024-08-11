873e497c453ab526eb4ac2093f4d7a175f0e6e83
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-11 00:17:32 -0400

    kube iam

--- Kube/iam.tf
+++ Kube/iam.tf
@@ -6,13 +6,13 @@ resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
   depends_on = [aws_eks_cluster.eks_cluster]
 }
 
-resource "aws_iam_role_policy" "eks_oidc_policy" {
-  name   = var.eks_oidc_policy
-  role   = var.githubactions-role-id
-  policy = templatefile("${path.module}/templates/eks_role_policy.json", { OIDC_ARN = aws_iam_openid_connect_provider.eks_oidc_provider.arn, OIDC_URL = replace(aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", ""), NAMESPACE = "kube-system", SA_NAME = "aws-node" })
+# resource "aws_iam_role_policy" "eks_oidc_policy" {
+#   name   = var.eks_oidc_policy
+#   role   = var.githubactions-role-id
+#   policy = templatefile("${path.module}/templates/eks_role_policy.json", { OIDC_ARN = aws_iam_openid_connect_provider.eks_oidc_provider.arn, OIDC_URL = replace(aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", ""), NAMESPACE = "kube-system", SA_NAME = "aws-node" })
 
-  depends_on = [aws_iam_openid_connect_provider.eks_oidc_provider]
-}
+#   depends_on = [aws_iam_openid_connect_provider.eks_oidc_provider]
+# }
 
 resource "aws_iam_role_policy_attachment" "aws_node" {
   role       = var.githubactions-role-name
