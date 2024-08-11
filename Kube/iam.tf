39320c3685aaa7c9ed67e4bb054a08f7ed3bc39c
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-11 00:19:15 -0400

    commented iam kube

--- Kube/iam.tf
+++ Kube/iam.tf
@@ -1,10 +1,10 @@
-resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks_oidc_url.certificates[0].sha1_fingerprint]
-  url             = data.tls_certificate.eks_oidc_url.url
+# resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = [data.tls_certificate.eks_oidc_url.certificates[0].sha1_fingerprint]
+#   url             = data.tls_certificate.eks_oidc_url.url
 
-  depends_on = [aws_eks_cluster.eks_cluster]
-}
+#   depends_on = [aws_eks_cluster.eks_cluster]
+# }
 
 # resource "aws_iam_role_policy" "eks_oidc_policy" {
 #   name   = var.eks_oidc_policy
