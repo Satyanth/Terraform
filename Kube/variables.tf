0368d6f50317af48f5d014b768112929b473585e
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-11 00:39:54 -0400

    kube variables

--- Kube/variables.tf
+++ Kube/variables.tf
@@ -1,37 +1,37 @@
-variable "eks-cluster-name" {
-  description = "Name of eks cluster"
-  type        = string
-  default     = "dev-eks-cluster"
-}
+# variable "eks-cluster-name" {
+#   description = "Name of eks cluster"
+#   type        = string
+#   default     = "dev-eks-cluster"
+# }
 
-variable "public_subnet_one_id" {
-  description = "subnet id for first subnet"
-  type        = string
-}
+# variable "public_subnet_one_id" {
+#   description = "subnet id for first subnet"
+#   type        = string
+# }
 
-variable "public_subnet_two_id" {
-  description = "subnet id for second subnet"
-  type        = string
-}
+# variable "public_subnet_two_id" {
+#   description = "subnet id for second subnet"
+#   type        = string
+# }
 
-variable "githubactions-role-arn" {
-  type        = string
-  description = "Github actions role arn"
-}
+# variable "githubactions-role-arn" {
+#   type        = string
+#   description = "Github actions role arn"
+# }
 
 variable "githubactions-role-name" {
   type        = string
   description = "Github actions role"
 }
 
-variable "eks_oidc_policy" {
-  type        = string
-  description = "Name for eks policy"
-  default     = "eks-oidc-policy"
-}
+# variable "eks_oidc_policy" {
+#   type        = string
+#   description = "Name for eks policy"
+#   default     = "eks-oidc-policy"
+# }
 
-variable "githubactions-role-id" {
-  type        = string
-  description = "Github actions role id"
-}
+# variable "githubactions-role-id" {
+#   type        = string
+#   description = "Github actions role id"
+# }
 
