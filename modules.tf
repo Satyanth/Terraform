66b50813f7f3aecf6aa71ec2365a8419b3c11f4b
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-11 00:34:58 -0400

    commented modules and outputs

--- modules.tf
+++ modules.tf
@@ -1,13 +1,13 @@
 module "base" {
   source             = "./Base"
-  githubactions-role = data.aws_iam_role.github-iam-role.id
+#   githubactions-role = data.aws_iam_role.github-iam-role.id
 }
 
 module "kube" {
   source                  = "./Kube"
-  public_subnet_one_id    = module.base.public_subnet_one_id
-  public_subnet_two_id    = module.base.public_subnet_two_id
-  githubactions-role-id   = data.aws_iam_role.github-iam-role.id
-  githubactions-role-arn  = data.aws_iam_role.github-iam-role.arn
-  githubactions-role-name = data.aws_iam_role.github-iam-role.name
+  # public_subnet_one_id    = module.base.public_subnet_one_id
+  # public_subnet_two_id    = module.base.public_subnet_two_id
+  # githubactions-role-id   = data.aws_iam_role.github-iam-role.id
+  # githubactions-role-arn  = data.aws_iam_role.github-iam-role.arn
+  # githubactions-role-name = data.aws_iam_role.github-iam-role.name
 }
