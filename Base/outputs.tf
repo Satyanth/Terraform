bb58429e4ebfe07d9d5a808ad2291d88ea58a2d3
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-11 00:43:57 -0400

    Building Infra

--- Base/outputs.tf
+++ Base/outputs.tf
@@ -1,27 +1,27 @@
-# output "vpc_id" {
-#   value = aws_vpc.vpc.id
-# }
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
 
-# output "public_subnet_one_id" {
-#   value = aws_subnet.public_subnet_1.id
-# }
+output "public_subnet_one_id" {
+  value = aws_subnet.public_subnet_1.id
+}
 
-# output "public_subnet_two_id" {
-#   value = aws_subnet.public_subnet_2.id
-# }
+output "public_subnet_two_id" {
+  value = aws_subnet.public_subnet_2.id
+}
 
-# output "private_subnet_id" {
-#   value = aws_subnet.private_subnet.id
-# }
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
 
-# output "public_routetable_one_id" {
-#   value = aws_route_table.public_subnet_one_route_table.id
-# }
+output "public_routetable_one_id" {
+  value = aws_route_table.public_subnet_one_route_table.id
+}
 
-# output "public_routetable_two_id" {
-#   value = aws_route_table.public_subnet_two_route_table.id
-# }
+output "public_routetable_two_id" {
+  value = aws_route_table.public_subnet_two_route_table.id
+}
 
-# output "private_routetable_id" {
-#   value = aws_route_table.private_subnet_route_table.id
-# }
+output "private_routetable_id" {
+  value = aws_route_table.private_subnet_route_table.id
+}
--- Kube/kubernetes.tf
+++ Kube/kubernetes.tf
@@ -1,8 +1,8 @@
-# resource "aws_eks_cluster" "eks_cluster" {
-#   name     = var.eks-cluster-name
-#   role_arn = var.githubactions-role-arn
-#   vpc_config {
-#     subnet_ids = [var.public_subnet_one_id, var.public_subnet_two_id]
-#   }
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.eks-cluster-name
+  role_arn = var.githubactions-role-arn
+  vpc_config {
+    subnet_ids = [var.public_subnet_one_id, var.public_subnet_two_id]
+  }
 
-# }
+}
