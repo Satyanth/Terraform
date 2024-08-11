740030f681d9223856e7d8e4da72c2457ed92dbe
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-11 00:33:07 -0400

    creating policy first

--- Kube/data_source.tf
+++ Kube/data_source.tf
@@ -1,3 +1,3 @@
-data "tls_certificate" "eks_oidc_url" {
-  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
-}
+# data "tls_certificate" "eks_oidc_url" {
+#   url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+# }
--- Kube/iam.tf
+++ Kube/iam.tf
@@ -1,18 +1,18 @@
-resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks_oidc_url.certificates[0].sha1_fingerprint]
-  url             = data.tls_certificate.eks_oidc_url.url
+# resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = [data.tls_certificate.eks_oidc_url.certificates[0].sha1_fingerprint]
+#   url             = data.tls_certificate.eks_oidc_url.url
 
-  depends_on = [aws_eks_cluster.eks_cluster]
-}
+#   depends_on = [aws_eks_cluster.eks_cluster]
+# }
 
-resource "aws_iam_role_policy" "eks_oidc_policy" {
-  name   = var.eks_oidc_policy
-  role   = var.githubactions-role-id
-  policy = templatefile("${path.module}/templates/eks_role_policy.json", { OIDC_ARN = aws_iam_openid_connect_provider.eks_oidc_provider.arn, OIDC_URL = replace(aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", ""), NAMESPACE = "kube-system", SA_NAME = "aws-node" })
+# resource "aws_iam_role_policy" "eks_oidc_policy" {
+#   name   = var.eks_oidc_policy
+#   role   = var.githubactions-role-id
+#   policy = templatefile("${path.module}/templates/eks_role_policy.json", { OIDC_ARN = aws_iam_openid_connect_provider.eks_oidc_provider.arn, OIDC_URL = replace(aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", ""), NAMESPACE = "kube-system", SA_NAME = "aws-node" })
 
-  depends_on = [aws_iam_openid_connect_provider.eks_oidc_provider]
-}
+#   depends_on = [aws_iam_openid_connect_provider.eks_oidc_provider]
+# }
 
 resource "aws_iam_role_policy_attachment" "aws_node" {
   role       = var.githubactions-role-name
