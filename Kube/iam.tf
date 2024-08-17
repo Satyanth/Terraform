2485b1509ac2ba15c54d6bf6deb59e399763a4ad
Satyant Kotipalli <satyanthsai@gmail.com>
2024-08-17 12:26:47 -0400

    main.tf

--- Kube/iam.tf
+++ Kube/iam.tf
@@ -1,11 +1,3 @@
-resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks_oidc_url.certificates[0].sha1_fingerprint]
-  url             = data.tls_certificate.eks_oidc_url.url
-
-  depends_on = [aws_eks_cluster.eks_cluster]
-}
-
 resource "aws_iam_role_policy_attachment" "aws_node" {
   role       = var.githubactions-role-name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks_oidc_url.certificates[0].sha1_fingerprint]
+  url             = data.tls_certificate.eks_oidc_url.url
+
+  depends_on = [aws_eks_cluster.eks_cluster]
+}
+
 resource "aws_iam_role" "github-actions-role" {
   name = var.githubactions-role-name
   assume_role_policy = jsonencode({
