b50414fb674b6e3c49c13912cc9290bc37059f7b
Satyant Kotipalli <satyanthsai@gmail.com>
2024-11-03 11:01:17 -0500

    testing workflow

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 resource "aws_iam_role" "github-actions-role" {
-    name = var.githubactions_role
-    assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Federated": "arn:aws:iam::298390376199:oidc-provider/token.actions.githubusercontent.com"
-            },
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Condition": {
-                "StringLike": {
-                    "token.actions.githubusercontent.com:sub": "repo:Satyanth/Terraform:*"
-                }
-            }
+  name = var.githubactions_role
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::298390376199:oidc-provider/token.actions.githubusercontent.com"
         },
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "eks.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:Satyanth/Terraform:*"
+          }
         }
+      },
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "eks.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
     ]
-    })
+  })
 }
